aaf0f697eb41b15a8a294b8990a5c122e94735e2
0xJoeMama <0xjoemama@gmail.com>
2024-04-21 13:09:49 +0300

    feat: publishing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -23,3 +23,24 @@ java {
         languageVersion = JavaLanguageVersion.of(17)
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                artifactId = "actually-tiny-remapper"
+            }
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "Repsy"
+            url = uri("https://repo.repsy.io/mvn/0xjoemama/public")
+            credentials {
+                username = System.getenv("REPSY_USERNAME")
+                password = System.getenv("REPSY_PASSWORD")
+            }
+        }
+    }
+}
